7490d56496e37b661bd3686b21061ed626e0fa9b
Xuan Hien <hiennguyen9874@gmail.com>
2022-01-20 23:57:36 +0700

    Update deepstream-notebook

--- deepstream-notebook/build_push.sh
+++ deepstream-notebook/build_push.sh
@@ -1,2 +1,2 @@
-DOCKER_BUILDKIT=1 docker build -t $1/deepstream-notebook:$3 --build-arg BASE_CONTAINER=$1/tensorrt-notebook:$2 .
+DOCKER_BUILDKIT=1 docker build -t $1/deepstream-notebook:$3 --build-arg BASE_CONTAINER=$1/tensorrt2-notebook:$2 .
 docker push $1/deepstream-notebook:$3
