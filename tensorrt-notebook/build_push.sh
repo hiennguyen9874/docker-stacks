192f7efd1e598df2d3267d27df2248b8ed36104d
Xuan Hien <hiennguyen9874@gmail.com>
2022-01-21 22:59:56 +0700

    Update tensorrt dockerfile

--- tensorrt-notebook/build_push.sh
+++ tensorrt-notebook/build_push.sh
@@ -1,2 +1,2 @@
-DOCKER_BUILDKIT=1 docker build -t $1/tensorrt-notebook:$3 --build-arg BASE_CONTAINER=$1/datascience-notebook:$2 .
+DOCKER_BUILDKIT=1 docker build -t $1/tensorrt-notebook:$3 --build-arg BASE_CONTAINER=$1/datascience-notebook:$2 CUDA_VERSION=$4 TRT_VERSION=$5 CUDA=$6 .
 docker push $1/tensorrt-notebook:$3
