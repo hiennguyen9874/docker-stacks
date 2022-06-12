790debf1cd87787fc0f7a4167b28518ab1b8ad5c
Xuan Hien <hiennguyen9874@gmail.com>
2022-06-12 12:20:23 +0700

    update deepstream

--- deepstream-notebook/build_push.sh
+++ deepstream-notebook/build_push.sh
@@ -1,2 +1,2 @@
-DOCKER_BUILDKIT=1 docker build -t $1/deepstream-notebook:$2 --build-arg BASE_CONTAINER=$3 .
+DOCKER_BUILDKIT=1 docker build -t $1/deepstream-notebook:$2 --build-arg BASE_CONTAINER=$3 --build-arg DISTRO=$4 --build-arg ARCH=$5 .
 docker push $1/deepstream-notebook:$2
